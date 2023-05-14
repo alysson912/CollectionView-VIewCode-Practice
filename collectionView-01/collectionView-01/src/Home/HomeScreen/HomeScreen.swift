0511bacfbff3693d8005c246cde5117906447c9e
Alysson Menezes <almenezes912@gmail.com>
2023-05-13 22:11:38 -0300

    projeto inicial configurado para ViewCode

---
+++ collectionView-01/collectionView-01/src/Home/HomeScreen/HomeScreen.swift
@@ -0,0 +1,22 @@
+//
+//  HomeScreen.swift
+//  collectionView-01
+//
+//  Created by ALYSSON DODO on 13/05/23.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+    
+    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ collectionView-01/collectionView-01/src/Home/HomeVC/HomeVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  collectionView-01
+//
+//  Created by ALYSSON DODO on 13/05/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    private var screen: HomeScreen?
+    
+    override func loadView() {
+        screen = HomeScreen()
+        view = screen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
